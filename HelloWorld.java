4c45c2c097da10ba6fd44e9fe1cf01cf310762d4
Antonio Caruso <antonio.mario.caruso@gmail.com>
2019-12-05 16:06:51 +0100

    Add HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args)  { 
+        System.out.println("Ciao");
+        System.out.println("Ragazzi");
+        System.out.print("Ciao "+"Ragazzi");
+        System.out.print("Siamo in ");
+        System.out.println(12+4);
+        System.out.println("12"+" 4");
+    }
+
+}
