85523150a755cdb462b81df1ef998c80caebd5ee
Antonio Caruso <antonio.mario.caruso@gmail.com>
2020-01-28 17:47:04 +0100

    Add files via upload
    Updated

--- Calcola.java
+++ Calcola.java
@@ -6,26 +6,65 @@ class Calcola {
         int n = l.length();
         char[] ch =  l.toCharArray();
         // Printing array
-        int[] numeri = new int[2];
+        int[] numeri = new int[10];
         int pos = 0;
+        
+        
+        char[] digits = new char[10];
+        int digits_pos = 0;
+     
+        /*
+        System.out.printf("%d\n",(int)digits[2]);
+        String s = new String(digits).trim();
+        System.out.println(Integer.parseInt(s));
+        System.exit(1);
+        */
+        
+        final int OUT = 0;
+        final int IN = 1; 
+        int stato = OUT;     
+        
+        
         char op = ' ';
         for (char c : ch) {
-            if (c == ' ') continue;
+            if (c == ' ') {
+                if (stato == IN) {
+                    for (int i=digits_pos; i<10; i++) digits[i] = ' ';
+                    numeri[pos++] = Integer.parseInt(new String(digits).trim());
+                    stato = OUT;
+                    digits_pos = 0;
+                }
+                continue;  
+            } 
             if (Character.isDigit(c)) {
-                numeri[pos++] = Character.digit(c,10);
+                digits[digits_pos++] = c; 
+                stato = IN;
             } else {
+                if (stato == IN) {
+                    for (int i=digits_pos; i<10; i++) digits[i] = ' ';
+                    numeri[pos++] = Integer.parseInt(new String(digits).trim());
+                    stato = OUT;
+                    digits_pos = 0;
+                }
                 switch (c) {
-                case '+':
-                case '-':
-                case '/':
-                case '*': op = c; break;
+                case '+': case '-': case '/': case '*': op = c;  break;
                 default: {
                     System.out.println("errore, operazione non valida");
                     System.exit(0);
-                }
+                   }
                 }
             }
         } 
+        
+        if (stato == IN)
+        {
+            for (int i=digits_pos; i<10; i++) digits[i] = ' ';
+            numeri[pos++] = Integer.parseInt(new String(digits).trim());
+            System.out.println(numeri[pos-1]);
+            stato = OUT;
+            digits_pos = 0;
+        }
+        
         System.out.print(l+" = ");
         switch (op) {
             case '+': System.out.println(numeri[0]+numeri[1]); break;
